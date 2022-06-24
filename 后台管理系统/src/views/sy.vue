ba7e9d1356654414069731a65f8f1a4464249c6e
king <1797009970@qq.com>
2022-06-24 21:37:51 +0800

    '作业'

---
+++ 后台管理系统/src/views/sy.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="">
+    <baidu-map
+      :center="center"
+      :zoom="zoom"
+      @ready="handler"
+      style="height: 1080px"
+      @click="getClickInfo"
+      :scroll-wheel-zoom="true"
+    >
+    </baidu-map>
+  </div>
+</template>
+<script>
+export default {
+  name: "TestBaiDu",
+  data() {
+    return {
+      center: { lng: 109.45744048529967, lat: 36.49771311230842 },
+      zoom: 6,
+    };
+  },
+  methods: {
+    handler({ BMap, map }) {
+      var point = new BMap.Point(109.49926175379778, 36.60449676862417);
+      map.centerAndZoom(point, 13);
+      var marker = new BMap.Marker(point); // 创建标注
+      map.addOverlay(marker); // 将标注添加到地图中
+      var circle = new BMap.Circle(point, 6, {
+        strokeColor: "Red",
+        strokeWeight: 6,
+        strokeOpacity: 1,
+        Color: "Red",
+        fillColor: "#f03",
+      });
+      map.addOverlay(circle);
+    },
+    getClickInfo(e) {
+      console.log(e.point.lng);
+      console.log(e.point.lat);
+      this.center.lng = e.point.lng;
+      this.center.lat = e.point.lat;
+    },
+  },
+};
+</script>
+<style lang="scss"></style>
